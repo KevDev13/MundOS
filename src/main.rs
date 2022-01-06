0d96816ba2eaeb21497bc84f7e977ef8a59e3692
Kevin Garner <kevin@kgar.net>
2022-01-06 15:41:25 -0600

    started work on tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![no_std]
 #![no_main]
 
+// testing needs
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::run_tests)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga;
 use vga::*;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, world!");
+    println!("Welcome to MundOS v{}.{}.{}", 0, 0, 1);
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,11 +28,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello, world!");
-    println!("Welcome to MundOS v{}.{}.{}", 0, 0, 1);
-    vga_color!(ColorCode::new(Color::Yellow, Color::Black));
-    println!("testing 123...");
-    loop {}
+/* Testing Functions */
+
+#[cfg(test)]
+fn run_tests(tests: &[&dyn Fn()]) {
+    println!("Running {} total tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assert() {
+    print!("Testing trivial... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
