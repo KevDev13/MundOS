39e391db9e3847fcf5a274e15bcf2b645cbe85b1
Kevin Garner <kevin@kgar.net>
2022-01-17 10:00:51 -0600

    double fault handled

--- src/gdt.rs
+++ src/gdt.rs
@@ -3,7 +3,9 @@ use x86_64::{
     structures::{
         tss::TaskStateSegment,
         gdt::{
-            GlobalDescriptorTable, Descriptor
+            GlobalDescriptorTable, 
+            Descriptor, 
+            SegmentSelector
         }
     }
 };
@@ -27,14 +29,29 @@ lazy_static! {
 }
 
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        gdt.add_entry(Descriptor::tss_segment(&TSS));
-        gdt
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (gdt, Selectors { code_selector, tss_selector })
     };
 }
 
 pub fn init() {
-    GDT.load();
+    use x86_64::instructions::{
+        segmentation::set_cs,
+        tables::load_tss
+    };
+
+    GDT.0.load();
+    
+    unsafe {
+        set_cs(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector);
+    }
+}
+
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
 }
